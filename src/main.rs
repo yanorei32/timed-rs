4341ccc5e144c6eeb695324c8f356516d525bb6a
yanorei32 <yanorei@hotmail.co.jp>
2025-04-30 19:20:09 +0900

    Use macro

--- src/main.rs
+++ src/main.rs
@@ -10,28 +10,26 @@ struct Cli {
     host: SocketAddr,
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+#[tokio::main(flavor = "current_thread")]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let rfc868_basetime = Utc.with_ymd_and_hms(1900, 1, 1, 0, 0, 0).unwrap();
-    let rt = tokio::runtime::Builder::new_current_thread().build()?;
     tracing_subscriber::fmt::init();
     let c = Cli::parse();
 
-    rt.block_on(async move {
-        let listener = TcpListener::bind(c.host).await?;
-        let addr = listener.local_addr()?;
-        tracing::info!("Server is ready on {addr}");
+    let listener = TcpListener::bind(c.host).await?;
+    let addr = listener.local_addr()?;
+    tracing::info!("Server is ready on {addr}");
 
-        loop {
-            let (mut socket, addr) = listener.accept().await?;
-            tracing::info!("Accept request from: {addr}");
+    loop {
+        let (mut socket, addr) = listener.accept().await?;
+        tracing::info!("Accept request from: {addr}");
 
-            tokio::spawn(async move {
-                let current_rfc868_time = (Utc::now() - rfc868_basetime).num_seconds() as i32;
+        tokio::spawn(async move {
+            let current_rfc868_time = (Utc::now() - rfc868_basetime).num_seconds() as i32;
 
-                if let Err(v) = socket.write_all(&current_rfc868_time.to_be_bytes()).await {
-                    tracing::info!("Something went wrong: {v}");
-                };
-            });
-        }
-    })
+            if let Err(v) = socket.write_all(&current_rfc868_time.to_be_bytes()).await {
+                tracing::info!("Something went wrong: {v}");
+            };
+        });
+    }
 }
